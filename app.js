80e42d6e138c5b7b4278f9741fd3985e579547c9
Renata Mie <renata_mie@hotmail.com>
2021-06-23 18:01:19 -0300

    change files

--- app.js
+++ app.js
@@ -20,21 +20,22 @@ app.get("/", function(req, res){
 
 
 app.get("/pecas", function(req, res){
+	res.render("pecas")
 
-	axios.get("https://graph.instagram.com/me/media?&fields=id,children{media_url},username,timestamp,caption,media_url,thumbnail_url,media_type,permalink&limit=200&access_token=" + process.env.TOKEN)
-	.then((response) => {
-		let postsArray = [];
+	// axios.get("https://graph.instagram.com/me/media?&fields=id,children{media_url},username,timestamp,caption,media_url,thumbnail_url,media_type,permalink&limit=200&access_token=" + process.env.TOKEN)
+	// .then((response) => {
+	// 	let postsArray = [];
 
-		response.data.data.map((posts) => {
-			postsArray.push(posts);
-		});
+	// 	response.data.data.map((posts) => {
+	// 		postsArray.push(posts);
+	// 	});
 
-		res.render("pecas", {posts:postsArray});
-	})
+	// 	res.render("pecas", {posts:postsArray});
+	// })
 
-	.catch((err) => {
-		console.log(err);
-	});
+	// .catch((err) => {
+	// 	console.log(err);
+	// });
 
 
 });
